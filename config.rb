ef57f75852980cdca72314bd1fff745d99b0690f
Steve Cherry <cherrys@cua.edu>
2014-01-15 11:03:41 -0500

    Make 404 page look like the blue screen of death

--- config.rb
+++ config.rb
@@ -67,7 +67,7 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-page "/404.html", :layout => false
+page "/404.html", :layout => false, :directory_index => false
 page "/sitemap.xml", :layout => false
 
 ###
