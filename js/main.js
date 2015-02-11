f9046e2685de6e6b0e932e15575f0d0fa5c346ec
Steve Cherry <stevenccherry@gmail.com>
2015-02-11 11:18:19 -0500

    Remove angular

--- js/main.js
+++ js/main.js
@@ -4,29 +4,4 @@
   $('.Navbar-toggle').click(function () {
     $('.Navbar-collapse').toggleClass('is-expanded');
   });
-
-
-  var app = angular.module('portfolioApp', []);
-
-  app.controller('ProjectController', [
-    '$scope',
-    '$http',
-    '$sce',
-
-    function ($scope, $http, $sce) {
-      $scope.projectPath = null;
-
-      $scope.$watch('projectPath', function () {
-        if ($scope.projectPath !== null) {
-          $http.get('projects/' + $scope.projectPath + '.html').success(function (data, status, headers, config) {
-            $scope.project = $sce.trustAsHtml(data);
-          });
-        }
-      });
-
-      this.set = function (path) {
-        $scope.projectPath = path;
-      };
-    }
-  ]);
 })();
