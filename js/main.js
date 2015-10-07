6573b56efc1b47bdc362e99d06a66ecb39463696
Steve Cherry <stevenccherry@gmail.com>
2015-10-07 15:53:25 -0400

    Add fluid styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 (function ($) {
-  $(document).foundation({
-    equalizer: {
-      equalize_on_stack: true
-    }
+  $(document).ready(function () {
+    $('table').wrap('<div class="u-scrollY"></div>');
+    $('iframe').wrap('<div class="FlexEmbed"></div>');
   });
 })(jQuery);
