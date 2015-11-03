150e5f77ff24f6aa2e71678854e2acf3c2b0d891
Steve Cherry <stevenccherry@gmail.com>
2015-11-02 20:56:44 -0500

    Use webfont loader for fonts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 (function ($) {
+  WebFont.load({
+    google: {
+      families: ['Source Sans Pro:400,400italic,700']
+    }
+  });
+
   $(document).ready(function () {
     $('table').wrap('<div class="u-scrollY"></div>');
     $('iframe').wrap('<div class="FlexEmbed"></div>');
