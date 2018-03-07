9d0cac9f49b3a13422e79c7575f3d2eda542350c
Steve Cherry <stevenccherry@gmail.com>
2018-03-07 18:37:20 -0500

    Scope the router to just the main content

--- js/main.js
+++ js/main.js
@@ -26,27 +26,26 @@
    */
 
   function render( pathname ) {
-    var body = document.querySelector('body');
+    var router = document.querySelector('.o-router');
     var title = document.querySelector('title');
+    var view = document.querySelector('.o-router__view');
 
-    body.classList.add('is-loading');
+    router.classList.add('is-loading');
 
     window.history.pushState( null, null, pathname );
 
     get( pathname, function( response ) {
       title.textContent = response.querySelector('title').textContent;
-      body.innerHTML = response.querySelector('body').innerHTML;
-
-      body.classList.remove('is-loading');
+      view.innerHTML = response.querySelector('.o-router__view').innerHTML;
+      router.classList.remove('is-loading');
     });
   }
 
-  function onRouteChange( e, target ) {
-    var pathname = target.pathname,
-      conditions = {
+  function onRouteChange( origin, pathname ) {
+    var conditions = {
         noPushState: !window.history.pushState,
-        //samePath: pathname === window.location.pathname,
-        differentOrigin: target.origin !== window.location.origin,
+        samePath: pathname === window.location.pathname,
+        differentOrigin: origin !== window.location.origin,
         isAsset: pathname.search(/\.(xml|css|js|png|jpg|svg)/) !== -1
       },
       any = Object.keys( conditions ).map(function( key ) {
@@ -59,11 +58,9 @@
       return;
     }
 
-    window.history.pushState( null, null, pathname );
+    window.history.pushState( { prev: window.location.pathname }, null, pathname );
 
     render( pathname );
-
-    e.preventDefault();
   }
 
   /**
@@ -84,12 +81,14 @@
     }
 
     if ( target !== document ) {
-      onRouteChange( e, target );
+      e.preventDefault();
+      onRouteChange( target.origin, target.pathname );
     }
   }
 
   function onPopstate( e ) {
-    onRouteChange( e, e.target.location );
+    e.preventDefault();
+    onRouteChange( e.target.location.origin, e.state.prev );
   }
 
   document.addEventListener( 'click', onClick );
