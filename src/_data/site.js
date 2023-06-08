2c53879eeecdc1ea7817b446b28aee2d2515dd18
Steve Cherry <steve@stevecherry.net>
2023-06-07 21:18:14 -0400

    Sync with starter template

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,12 @@
 const pkg = require('../../package.json');
 
-module.exports = () => {
+module.exports = function () {
   return {
-    env: process.env.ELEVENTY_RUN_MODE === 'build' ? 'production' : 'development',
+    env:
+      process.env.ELEVENTY_RUN_MODE === 'build' ? 'production' : 'development',
     name: pkg.author.name,
     color: '#2b8a3e',
+    backgroundColor: '#ffffff',
     github_username: pkg.name.split('.')[0],
   };
 };
