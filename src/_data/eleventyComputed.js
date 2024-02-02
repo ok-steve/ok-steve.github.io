cf58788e73ae4845d9417ff62a54427e4ea9de71
ok-steve <steve@stevecherry.net>
2024-02-02 14:34:56 -0500

    Make more default data global

---
+++ src/_data/eleventyComputed.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+  return {
+    date({ site, date }) {
+      if (date) return date;
+      // In prod use Git for the default date.
+      if (site.env === 'production') return 'git Created';
+      return undefined;
+    },
+    permalink({ site, permalink }) {
+      // Render unpublished pages in dev but not on prod.
+      if (site.env === 'production' && permalink === false) return false;
+
+      // Otherwise, replicate 11ty's default permalink behavior.
+      if (permalink) return permalink;
+      return undefined;
+    },
+  };
+};
--- src/writing/writing.11tydata.js
+++ src/writing/writing.11tydata.js
@@ -1,12 +1,4 @@
 module.exports = {
   layout: 'writing',
   tags: ['writing'],
-  eleventyComputed: {
-    // Prevent unpublished pages from being rendered on production
-    permalink({ site, permalink, page }) {
-      if (site.env === 'production' && permalink === false) return false;
-      if (permalink) return permalink;
-      return `${page.filePathStem}/`;
-    },
-  },
 };
