dbaeb951b082221ccef208a08ead7e776cf64dfb
ok-steve <steve@stevecherry.net>
2024-02-23 11:50:13 -0500

    Remove prettierrc

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 const {
   EleventyHtmlBasePlugin,
   EleventyRenderPlugin,
-} = require('@11ty/eleventy');
-const EleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const EleventyRssPlugin = require('@11ty/eleventy-plugin-rss');
-const EleventySyntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight');
+} = require("@11ty/eleventy");
+const EleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const EleventyRssPlugin = require("@11ty/eleventy-plugin-rss");
+const EleventySyntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   /**
@@ -21,39 +21,39 @@ module.exports = function (eleventyConfig) {
    * Passthrough copy
    */
 
-  eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy({
-    './node_modules/prismjs/themes/prism-solarizedlight.css':
-      './public/syntax-highlighting.css',
+    "./node_modules/prismjs/themes/prism-solarizedlight.css":
+      "./public/syntax-highlighting.css",
   });
-  eleventyConfig.addPassthroughCopy('./public');
-  eleventyConfig.addPassthroughCopy('./src/*.{txt,xml}');
-  eleventyConfig.addPassthroughCopy('./src/sw.js');
+  eleventyConfig.addPassthroughCopy("./public");
+  eleventyConfig.addPassthroughCopy("./src/*.{txt,xml}");
+  eleventyConfig.addPassthroughCopy("./src/sw.js");
 
-  eleventyConfig.addWatchTarget('./src/code/**/*.{css,js}');
+  eleventyConfig.addWatchTarget("./src/code/**/*.{css,js}");
 
   /**
    * Libraries
    */
 
-  ['md'].forEach((name) =>
+  ["md"].forEach((name) =>
     eleventyConfig.setLibrary(name, require(`./lib/libraries/${name}`))
   );
 
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    excerpt_separator: '<!-- excerpt -->',
+    excerpt_separator: "<!-- excerpt -->",
   });
 
   /**
    * Shortcodes
    */
 
-  ['audio'].forEach((name) =>
+  ["audio"].forEach((name) =>
     eleventyConfig.addShortcode(name, require(`./lib/shortcodes/${name}`))
   );
 
-  ['youtube'].forEach((name) =>
+  ["youtube"].forEach((name) =>
     eleventyConfig.addNunjucksAsyncShortcode(
       name,
       require(`./lib/shortcodes/${name}`)
@@ -61,11 +61,11 @@ module.exports = function (eleventyConfig) {
   );
 
   return {
-    markdownTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
     dir: {
-      input: 'src',
-      output: 'dist',
-      layouts: '_layouts',
+      input: "src",
+      output: "dist",
+      layouts: "_layouts",
     },
   };
 };
