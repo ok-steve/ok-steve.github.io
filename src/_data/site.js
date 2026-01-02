d069d1ecf8cc6b5e91139c77ec8ac49048180746
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2026-01-02 17:30:33 +0000

    Clean up code

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,10 @@
-// import pkg from "../../package.json";
-
 export default function () {
   return {
     env:
       process.env.ELEVENTY_RUN_MODE === "build" ? "production" : "development",
-    // name: pkg.author.name,
     name: "Steve Cherry",
     color: "#2b8a3e",
     backgroundColor: "#ffffff",
-    // github_username: pkg.name.split(".")[0],
     github_username: "ok-steve",
   };
 }
